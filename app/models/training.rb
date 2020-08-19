d67f3255b716e1b702e79f790901e81c031fcb51
jampanman-git <kosukengineer@gmail.com>
2020-08-20 00:26:18 +0900

    Trainingモデル バリデーション設定

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,2 +1,15 @@
 class Training < ApplicationRecord
+  with_options presence: true do
+    validates :arm
+    validates :spine
+    validates :abs
+    validates :leg
+  end
+
+  with_options format: { with: /\A[0-9]+\z/, message: "半角数字で入力してください"} do
+    validates :arm
+    validates :spine
+    validates :abs
+    validates :leg
+  end
 end
