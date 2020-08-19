46599911609d85cf8967de7c2ca1139b78435203
jampanman-git <kosukengineer@gmail.com>
2020-08-20 04:27:14 +0900

    userコントローラーindexアクション変更

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,5 +1,10 @@
 class TrainingsController < ApplicationController
   def index
-    @training = Training.where(user_id: current_user.id).limit(1).order("created_at DESC")
-  end
+    if user_signed_in?
+      @training = Training.where(user_id: current_user.id).limit(1).order("created_at DESC")
+      # if @items.present?
+        @data = {'腕筋' => @training.arm , '背筋' => @training.spine, '腹筋' => @training.abs,  '脚筋' =>@training.leg }
+      # end
+    end
+  end 
 end
