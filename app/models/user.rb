354faea7a825ca14f72dda3e9a81600210590a12
jampanman-git <kosukengineer@gmail.com>
2020-08-19 17:37:32 +0900

    Userモデル単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname, uniqueness: true, length: { maximum: 6 }
     validates :email, uniqueness: true
-    validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i, password: "は半角英数字で入力してください"}
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, password: "は半角英数字混合で入力してください"}
   end
 
   # Include default devise modules. Others available are:
