be98be580f62a02b2e7eb41961f9250339a14773
jampanman-git <kosukengineer@gmail.com>
2020-08-20 05:13:32 +0900

    ログイン時のページ表示のための条件分岐

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -2,9 +2,9 @@ class TrainingsController < ApplicationController
   def index
     if user_signed_in?
       @training = Training.where(user_id: current_user.id).limit(1).order("created_at DESC")
-      # if @items.present?
+      if @items.present?
         @data = {'腕筋' => @training.arm , '背筋' => @training.spine, '腹筋' => @training.abs,  '脚筋' =>@training.leg }
-      # end
+      end
     end
   end 
 end
