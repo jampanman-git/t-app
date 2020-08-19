816989146f7b34886af609011d67eafadd04aebc
jampanman-git <kosukengineer@gmail.com>
2020-08-19 13:34:34 +0900

    nicknameバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
   with_options presence: true do
-    validates :nickname, uniqueness: true, length: { minimum: 6 }
+    validates :nickname, uniqueness: true, length: { maximum: 6 }
     validates :email, uniqueness: true
     validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i, password: "は半角英数字で入力してください"}
   end
