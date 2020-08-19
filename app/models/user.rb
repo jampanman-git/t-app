23c19d779550f99178eb27df446b788df0c48847
jampanman-git <kosukengineer@gmail.com>
2020-08-19 12:39:44 +0900

    Userモデル バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+
+  with_options presence: true do
+    validates :nickname, uniqueness: true, length: { minimum: 6 }
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/i, password: "は半角英数字で入力してください"}
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
