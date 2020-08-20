5a5528c3b165d4f8ec2ad5a5b2973f2f068dcfe6
jampanman-git <kosukengineer@gmail.com>
2020-08-20 17:14:28 +0900

    ストロングパラメーター修正

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -13,18 +13,17 @@ class TrainingsController < ApplicationController
   end
 
   def create
-    @training = Training.new(training_params)
-    if @training.save
-      redirect_to root_path
-    else
-      render:new
-    end
+    @training.create(training_params)
+    #   redirect_to root_path
+    # else
+    #   render:new
+    # end
   end
 
   private
 
   def training_params
-    params.require(:training).permit(:arm, :spine, :abs, :legs).merge(user_id: current_user.id)
+    params.require(:training).permit(:arm, :spine, :abs, :leg).merge(user_id: current_user.id)
   end
 
 end
