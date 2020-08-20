a19117eaf0fd662948cdd5c226a56585a54794a1
jampanman-git <kosukengineer@gmail.com>
2020-08-20 15:30:53 +0900

    new&createアクション ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :trainings, only: [:index]
+  resources :trainings, only: [:index, :new, :create]
   root "trainings#index"
 end
