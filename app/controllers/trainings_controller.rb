466cf4ea3648dab00522b2faf594d9a12b57800e
jampanman-git <kosukengineer@gmail.com>
2020-08-21 18:06:55 +0900

    過去１週間のトレーニング部位割合の実装

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,11 +1,15 @@
 class TrainingsController < ApplicationController
   def index
-    range=Range.new(Time.zone.today, Time.zone.tomorrow)
-    # binding.pry
+    range=Range.new(Time.zone.today, Time.zone.today.tomorrow)
     @trainings = Training.where(created_at: range, user_id: current_user.id)
     @data = {'腕筋' => @trainings.sum(:arm) , '背筋' => @trainings.sum(:spine), '腹筋' => @trainings.sum(:abs),  '脚筋' => @trainings.sum(:leg)}
 
-    # range2=Range.new(,Time.zone.today.tomorrow)
+    range2=Range.new(Time.zone.today.ago(6.days),Time.zone.tomorrow)
+    @trainings2 = Training.where(created_at: range2, user_id: current_user.id)
+    @data2 = {'腕筋' => @trainings2.sum(:arm) , '背筋' => @trainings2.sum(:spine), '腹筋' => @trainings2.sum(:abs),  '脚筋' => @trainings2.sum(:leg)}
+    binding.pry
+    
+    # @trainings3 = Training.where(created_at: range, user_id: current_user.id)
   end 
 
   def new
