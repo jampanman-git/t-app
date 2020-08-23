7d8a455e73d1116d0432dbdf9e1fe87f92202883
jampanman-git <kosukengineer@gmail.com>
2020-08-23 16:48:09 +0900

    ルーティングコメントアウト解除

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   resources :trainings, only: [:index, :new, :create]
   root "trainings#index"
 
-  # put 'users/follow/:user_id' => 'users#follow'
-  # put 'users/unfollow/:user_id' => 'users#unfollow'
-  # #フォローとフォローを外すアクション
+  put 'users/follow/:user_id' => 'users#follow'
+  put 'users/unfollow/:user_id' => 'users#unfollow'
+  #フォローとフォローを外すアクション
 
-  # get 'users/follow_list/:user_id' => 'users#follow_list'
-  # get 'users/follower_list/:user_id' => 'users#follower_list'
-  #フォロー・フォロワーの一覧ページ
+  get 'users/follow_list/:user_id' => 'users#follow_list'
+  get 'users/follower_list/:user_id' => 'users#follower_list'
+  フォロー・フォロワーの一覧ページ
 end
