8bbbe47d50926ca5e97fcb773fd1c6a883e65edb
jampanman-git <kosukengineer@gmail.com>
2020-08-23 17:14:51 +0900

    Userモデルファイルのインデント整理

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  acts_as_follower
   acts_as_followable
+  acts_as_follower
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   resources :trainings, only: [:index, :new, :create]
   root "trainings#index"
 
-  put 'users/follow/:user_id' => 'users#follow'
-  put 'users/unfollow/:user_id' => 'users#unfollow'
-  #フォローとフォローを外すアクション
-
-  get 'users/follow_list/:user_id' => 'users#follow_list'
-  get 'users/follower_list/:user_id' => 'users#follower_list'
-  フォロー・フォロワーの一覧ページ
+  put 'users/follow/:user_id',to: 'users#follow'
+  put 'users/unfollow/:user_id',to: 'users#unfollow'
+  get 'users/follow_list/:user_id',to: 'users#follow_list'
+  get 'users/follower_list/:user_id',to:'users#follower_list'
+  # フォロー・フォロワーの一覧ページ
 end
