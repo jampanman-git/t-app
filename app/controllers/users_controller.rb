5c95a74d2a468424f7e3247427c5d255199e309c
jampanman-git <kosukengineer@gmail.com>
2020-08-24 16:57:39 +0900

    フォローボタン実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,11 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all.order(nickname: "DESC")
+    # @user = User.find(params[:id])
   end
 
-  def show
-  end
+  # def show
+  # end
 
   def follow
     @user = User.find(params[:user_id])
