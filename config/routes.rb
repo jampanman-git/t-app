35f12c2bdaed87de6312a448b8c2c68f67c4e996
jampanman-git <kosukengineer@gmail.com>
2020-08-24 14:36:05 +0900

    Userモデル ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
   
   resources :trainings, only: [:index, :new, :create]
   root "trainings#index"
