47a2beabc03e0b8ea68297694f619d24f8089b1a
jampanman-git <kosukengineer@gmail.com>
2020-08-25 11:26:11 +0900

    searchルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    collection do
+      get 'search'
+    end
+  end
   
   resources :trainings, only: [:index, :new, :create]
   root "trainings#index"
