3f99575cbbb2856cf595c49a7a890eb793142cfd
jampanman-git <kosukengineer@gmail.com>
2020-08-27 02:22:03 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:password])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:password])
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname,:email,:image])
+  end
 end
