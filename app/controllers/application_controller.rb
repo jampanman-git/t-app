a2edcccce4af779753c8aad7eff443e9eebf8749
jampanman-git <kosukengineer@gmail.com>
2020-08-27 01:39:53 +0900

    Revert "ストロングパラメーター実装"
    This reverts commit 4939ce593a10572768963bf7d91e1b85ad87909d.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:password,:image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname,:email,:image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:password])
   end
 end
