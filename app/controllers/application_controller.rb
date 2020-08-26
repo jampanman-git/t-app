4939ce593a10572768963bf7d91e1b85ad87909d
jampanman-git <kosukengineer@gmail.com>
2020-08-27 01:06:12 +0900

    ストロングパラメーター実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:password,:image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname,:email,:image])
   end
 end
