b22dd11d38dc24d369b2a07e604f23320ed5f219
jampanman-git <kosukengineer@gmail.com>
2020-08-28 15:33:03 +0900

    followsテーブルから特定レコードの指定

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -42,6 +42,9 @@ class TrainingsController < ApplicationController
                 Time.zone.today.ago(4.days).strftime('%Y年%m月%d日') => @total5,Time.zone.today.ago(3.days).strftime('%Y年%m月%d日') => @total4,
                 Time.zone.today.ago(2.days).strftime('%Y年%m月%d日') => @total3,Time.zone.today.ago(1.days).strftime('%Y年%m月%d日') => @total2,
                 Time.zone.today.strftime('%Y年%m月%d日') => @total1 }
+
+      @test = Follow.where(created_at: range,foller_id: current_user.id)
+      binding.pry
     end
   end 
 
